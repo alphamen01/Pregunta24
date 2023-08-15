0d38ae6facd52f567e1f8d27378c1962845e1f95
alphamen01 <lesg.2018@hotmail.com>
2023-08-15 12:16:35 -0500

    inicio

---
+++ Pregunta 24/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //INGRESAMOS EL NUMERO
+        Console.Write("Ingresa un número: ");
+        int numero = int.Parse(Console.ReadLine()!);
+
+        //INGRESAMOS LA CANTIDAD DE VECES EN QUE SE VA SUMAR EL NRO
+        Console.Write("Ingresa la cantidad de veces a sumar: ");
+        int n = int.Parse(Console.ReadLine()!);
+
+        int resultado = SumarNCantidades(numero, n);
+
+        Console.WriteLine("La suma de " + n + " veces el número " + numero + " es: " + resultado);
+        //Console.WriteLine($"La suma de {n} veces el número {numero} es: {resultado}");
+    }
+        public static int SumarNCantidades(int numero, int n)
+        {
+            int suma = 0;
+            for (int i = 0; i < n; i++)
+            {
+                //suma += numero; abreviado
+                suma = suma + numero;
+            }
+            return suma;
+        }
+
+}
